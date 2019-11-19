68952e0626f4ccbbd4c1eb198c20501925ba7bbc
novakvova <novakvova@gmail.com>
2019-11-19 18:59:12 +0200

    Test login

--- login.php
+++ login.php
@@ -1,13 +1,41 @@
-<?php include "_header.php"; ?>
+<?php
+    $errors=array();
+    $email='';
+    if($_SERVER["REQUEST_METHOD"]=="POST")
+    {
+        if(isset($_POST['email']) and !empty($_POST['email']))
+        {
+            $email=$_POST['email'];
+        }
+        else
+            $errors["email"]="Поле пошта є обов'язковим";
 
+        if(count($errors)==0) {
+            header("Location: /index.php");
+            exit;
+        }
+    }
+?>
+
+<?php include "_header.php"; ?>
 
 <div class="login-container">
             <div class="row">
                 <div class="offset-md-3 col-md-6 login-form-1">
                     <h3>Login for Form 1</h3>
-                    <form>
+                    <?php if(count($errors)!=0) { ?>
+                        <div class="alert alert-danger" role="alert">
+                            Заповніть поля
+                        </div>
+                    <?php } ?>
+
+                    <form method="post">
                         <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Your Email *" value="" />
+                            <input type="text" 
+                                class="form-control" 
+                                name="email"
+                                placeholder="Your Email *" 
+                                value="" />
                         </div>
                         <div class="form-group">
                             <input type="password" class="form-control" placeholder="Your Password *" value="" />
